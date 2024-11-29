9a4e2d91412b02380bbad6de18cb6d1356840e6a
hartmaj2 <135450351+hartmaj2@users.noreply.github.com>
2024-11-30 00:32:10 +0100

    Add tile generation

--- scripts/environment.gd
+++ scripts/environment.gd
@@ -17,4 +17,40 @@ func _ready() -> void:
 			row.append(TileTypes.MEADOW)
 		terrain.append(row)
 	
-	print(terrain)
+	
+	initialize_randomly()
+	set_map()
+	#print_terrain()
+	
+	
+	
+func _input(event):
+	# Mouse in viewport coordinates.
+	
+	if event is InputEventMouseButton:
+		var pos = $TileMapLayer.local_to_map(get_local_mouse_position())
+		print("Position on map",pos)
+		#$TileMapLayer.set_cell(pos,4,Vector2i(0,0))
+		
+
+func initialize_randomly() -> void:
+	for i in range(ENV_SIZE):
+		for j in range(ENV_SIZE):
+			terrain[i][j] = randi_range(0,2)
+	pass
+
+func set_map() -> void:
+	for i in range(ENV_SIZE):
+		for j in range(ENV_SIZE):
+			print("Setting cell to",terrain[i][j])
+			$TileMapLayer.set_cell(Vector2i(i,j),get_tile_id(terrain[i][j]),Vector2i(0,0))
+
+func get_tile_id(type : TileTypes) -> int:
+	if type == TileTypes.DESERT:
+		return 3
+	elif type == TileTypes.MEADOW:
+		return 4
+	elif type == TileTypes.WATER:
+		return 5
+	else:
+		return -1
