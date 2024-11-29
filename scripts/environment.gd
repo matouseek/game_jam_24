f7f870c39edceeefe2a2b6a0682984e04002ac33
matouseek <matouszatloukal@seznam.cz>
2024-11-29 23:22:38 +0100

    Add terrain representation + fix tilemap coord

---
+++ scripts/environment.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+const ENV_SIZE: int = 12
+
+enum TileTypes {
+	DESERT,
+	MEADOW,
+	WATER
+}
+
+var terrain: Array = []
+
+func _ready() -> void:
+	for i in range(ENV_SIZE):
+		var row: Array = []
+		for j in range(ENV_SIZE):
+			row.append(TileTypes.MEADOW)
+		terrain.append(row)
+	
+	print(terrain)
