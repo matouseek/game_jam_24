37ca8872277e88e7254e1dbd67e730a9928ff07f
SADOVSKV <3217cody@gmail.com>
2024-11-30 12:52:17 +0100

    Delete shits

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,10 +1,10 @@
 extends CanvasLayer
 var MAX_SHARE = 0.5
-@onready var action_nodes = {
-	PS.PlayerEffects.RAIN : $Actions/Rain/Sprite2D,
-	PS.PlayerEffects.DRAUGHT : $Actions/Draught/Sprite2D,
-	PS.PlayerEffects.MAJORITY : $Actions/Majority/Sprite2D
-}
+#@onready var action_nodes = {
+	#PS.PlayerEffects.RAIN : $Actions/Rain/Sprite2D,
+	#PS.PlayerEffects.DRAUGHT : $Actions/Draught/Sprite2D,
+	#PS.PlayerEffects.MAJORITY : $Actions/Majority/Sprite2D
+#}
 
 @onready var goals_nodes = {
 	G.TileTypes.WATER : $Goals/Water/Sprite2D,
@@ -21,31 +21,31 @@ var MAX_SHARE = 0.5
 
 
 func _ready() -> void:
-	update_hud()
+	#update_hud()
 	$ItemList.select(0)
 
-func _on_rain_pressed() -> void:
-	reset_hud()
-	PS.update_state(PS.PlayerEffects.RAIN)
-	update_hud()
-
-func _on_draught_pressed() -> void:
-	reset_hud()
-	PS.update_state(PS.PlayerEffects.DRAUGHT)
-	update_hud()
-
-
-func _on_majority_pressed() -> void:
-	reset_hud()
-	PS.update_state(PS.PlayerEffects.MAJORITY)
-	update_hud()
+#func _on_rain_pressed() -> void:
+	#reset_hud()
+	#PS.update_state(PS.PlayerEffects.RAIN)
+	#update_hud()
+#
+#func _on_draught_pressed() -> void:
+	#reset_hud()
+	#PS.update_state(PS.PlayerEffects.DRAUGHT)
+	#update_hud()
+#
+#
+#func _on_majority_pressed() -> void:
+	#reset_hud()
+	#PS.update_state(PS.PlayerEffects.MAJORITY)
+	#update_hud()
 	
 
-func update_hud():
-	action_nodes[PS.selected_effect].modulate = Color(1, 0, 0)
-	
-func reset_hud():
-	action_nodes[PS.selected_effect].modulate = Color(1, 1, 1)
+#func update_hud():
+	#action_nodes[PS.selected_effect].modulate = Color(1, 0, 0)
+	#
+#func reset_hud():
+	#action_nodes[PS.selected_effect].modulate = Color(1, 1, 1)
 
 func set_goal(type: G.TileTypes, value:float):
 	var sprite = goals_nodes[type]
@@ -58,6 +58,6 @@ func update_progress(vals):
 		progress_nodes[i].value = vals[i]*100
 
 func _on_item_list_item_clicked(index: int, at_position: Vector2, mouse_button_index: int) -> void:
-	reset_hud()
+	#reset_hud()
 	PS.update_state(index)
-	update_hud()
+	#update_hud()
