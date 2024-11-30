a0259450dc6e2e75db0406d38c0c76daa69c0636
SADOVSKV <3217cody@gmail.com>
2024-11-30 19:33:31 +0100

    Second music works

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -26,6 +26,7 @@ func _ready() -> void:
 
 func anihilation():
 	$ColorRect.visible = false
+	AS.play_music("res://assets/Sounds/game_placeholder.mp3")
 
 func _input(event: InputEvent) -> void:
 	if (event.is_action_pressed("zoom_in")):
