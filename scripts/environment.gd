feec7407aaa77879fbe2da95384fba5ada1dbacf
SADOVSKV <3217cody@gmail.com>
2024-11-30 09:22:12 +0100

    Polish highlighting code

--- scripts/environment.gd
+++ scripts/environment.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 const ENV_SIZE: int = 12
-var last_pos = [Vector2i(-1000,-1000)]
+var last_selected = [Vector2i(-1000,-1000)]
 
 var offsets = [Vector2i(-1, -1), Vector2i(0, -1), Vector2i(1, -1),
 		Vector2i(-1,  0),               Vector2i(1,  0),
@@ -27,26 +27,43 @@ func _ready() -> void:
 	
 	
 func _input(event):
-	# Mouse in viewport coordinates.
 	if (event is InputEventMouse):
 		var pos = $TileMapLayer.local_to_map(get_local_mouse_position())
-		if (pos.x >= 0 and pos.x < ENV_SIZE and pos.y >= 0 and pos.y<ENV_SIZE):
+		if (is_valid_click_pos(pos)):
 			if event.is_action_pressed("mouse_click"):
-				for lpos in last_pos:
-					$Used.set_cell(lpos,PS.selected_effect,Vector2i(0,0))
-					terrain[lpos.x][lpos.y] = 2
-			if (last_pos != null):
-				for lpos in last_pos:
-					$Highlight.set_cell(lpos,-1,Vector2i(0,0))
-			last_pos = [pos]
-			$Highlight.set_cell(pos,PS.selected_effect,Vector2i(0,0))
-			if (PS.PlayerEffects.MAJORITY == PS.selected_effect):
-				for offset in offsets:
-					var newpos = pos+offset
-					if (newpos.x >= 0 and newpos.x < ENV_SIZE and newpos.y >= 0 and newpos.y<ENV_SIZE):
-						last_pos.append(newpos)
-						$Highlight.set_cell(newpos,PS.selected_effect,Vector2i(0,0))
-	
+				confirm_tile_selection()	
+			handle_highlights(pos)
+	if Input.is_action_pressed("ui_accept"):
+		erase_all_effects()
+
+func is_valid_click_pos(pos : Vector2i) -> bool:
+	return pos.x >= 0 and pos.x < ENV_SIZE and pos.y >= 0 and pos.y< ENV_SIZE
+
+func confirm_tile_selection() -> void:
+	for lpos in last_selected: # confirm selection
+		$Used.set_cell(lpos,PS.selected_effect,Vector2i(0,0))
+
+func handle_highlights(pos: Vector2i) -> void:
+	if (last_selected[0] != pos): # check if cursor moved to new location
+		for lpos in last_selected:
+			$Highlight.set_cell(lpos,-1,Vector2i(0,0))
+		last_selected = [pos]
+		
+		$Highlight.set_cell(pos,PS.selected_effect,Vector2i(0,0))
+		if (PS.PlayerEffects.MAJORITY == PS.selected_effect):
+			highligh_neighbors(pos)
+
+func highligh_neighbors(pos : Vector2i) -> void:
+	for offset in offsets:
+		var newpos = pos+offset
+		if (newpos.x >= 0 and newpos.x < ENV_SIZE and newpos.y >= 0 and newpos.y<ENV_SIZE):
+			last_selected.append(newpos)
+			$Highlight.set_cell(newpos,PS.selected_effect,Vector2i(0,0))
+
+func erase_all_effects() -> void:
+	for i in range(ENV_SIZE):
+		for j in range(ENV_SIZE):
+			$Used.set_cell(Vector2i(i,j),-1,Vector2i(0,0))
 
 func initialize_randomly() -> void:
 	for i in range(ENV_SIZE):
