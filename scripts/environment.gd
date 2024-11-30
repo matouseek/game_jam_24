83fc25f56a76ad95c003739afc5963163c129e41
SADOVSKV <3217cody@gmail.com>
2024-11-30 03:09:45 +0100

    Insanely shitty tile highlighting done

--- scripts/environment.gd
+++ scripts/environment.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 const ENV_SIZE: int = 12
+var last_pos = null
+var last_id = 0
 
 enum TileTypes {
 	DESERT,
@@ -23,11 +25,16 @@ func _ready() -> void:
 	
 func _input(event):
 	# Mouse in viewport coordinates.
-	
+	var pos = $TileMapLayer.local_to_map(get_local_mouse_position())
 	if event.is_action_pressed("mouse_click"):
-		var pos = $TileMapLayer.local_to_map(get_local_mouse_position())
 		print("Position on map",pos)
 		$TileMapLayer.set_cell(pos,4,Vector2i(0,0))
+	if (pos.x >= 0 and pos.x < ENV_SIZE and pos.y >= 0 and pos.y<ENV_SIZE and last_pos!=pos):
+		if (last_pos != null):
+			$TileMapLayer.set_cell(last_pos,last_id,Vector2i(0,0))
+		last_id = $TileMapLayer.get_cell_source_id(pos)
+		last_pos = pos
+		$TileMapLayer.set_cell(pos,3,Vector2i(0,0))
 		
 
 func initialize_randomly() -> void:
@@ -39,12 +46,12 @@ func initialize_randomly() -> void:
 func set_map() -> void:
 	for i in range(ENV_SIZE):
 		for j in range(ENV_SIZE):
-			print("Setting cell to",terrain[i][j])
-			$TileMapLayer.set_cell(Vector2i(i,j),get_tile_id(terrain[i][j]),Vector2i(0,0))
+			#print("Setting cell to",terrain[i][j])
+			$TileMapLayer.set_cell(Vector2i(i,j),terrain[i][j],Vector2i(0,0))
 
 func get_tile_id(type : TileTypes) -> int:
 	if type == TileTypes.DESERT:
-		return 3
+		return 0
 	elif type == TileTypes.MEADOW:
 		return 4
 	elif type == TileTypes.WATER:
