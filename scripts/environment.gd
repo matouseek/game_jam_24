6aa0c6124ab8c975111bd15ecf98d3317554c99c
SADOVSKV <3217cody@gmail.com>
2024-11-30 05:36:37 +0100

    Prepared front side algorithm

--- scripts/environment.gd
+++ scripts/environment.gd
@@ -33,25 +33,27 @@ func _input(event):
 		if (pos.x >= 0 and pos.x < ENV_SIZE and pos.y >= 0 and pos.y<ENV_SIZE):
 			if event.is_action_pressed("mouse_click"):
 				for lpos in last_pos:
-					$TileMapLayer.set_cell(lpos,2,Vector2i(0,0))
+					$Used.set_cell(lpos,PS.selected_effect,Vector2i(0,0))
 					terrain[lpos.x][lpos.y] = 2
 			if (last_pos != null):
 				for lpos in last_pos:
 					$Highlight.set_cell(lpos,-1,Vector2i(0,0))
 			last_pos = [pos]
-			$Highlight.set_cell(pos,0,Vector2i(0,0))
+			$Highlight.set_cell(pos,PS.selected_effect,Vector2i(0,0))
 			if (PS.PlayerEffects.MAJORITY == PS.selected_effect):
 				for offset in offsets:
 					var newpos = pos+offset
 					if (newpos.x >= 0 and newpos.x < ENV_SIZE and newpos.y >= 0 and newpos.y<ENV_SIZE):
 						last_pos.append(newpos)
-						$Highlight.set_cell(newpos,0,Vector2i(0,0))
+						$Highlight.set_cell(newpos,PS.selected_effect,Vector2i(0,0))
 	
 
 func initialize_randomly() -> void:
 	for i in range(ENV_SIZE):
 		for j in range(ENV_SIZE):
-			terrain[i][j] = randi_range(0,1)
+			if (j == ENV_SIZE-1 or i == ENV_SIZE-1): terrain[i][j] = 2
+			if (j== ENV_SIZE-1 and i == ENV_SIZE-1): terrain[i][j] = -1
+			else: terrain[i][j] = randi_range(0,1)
 
 func set_map() -> void:
 	for i in range(ENV_SIZE):
